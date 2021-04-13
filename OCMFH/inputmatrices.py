9b0d104f7a883bfba7fa06f5404ab0657fa3c89b
dwijesh522 <iddwijesh@gmail.com>
2021-04-13 23:20:30 +0530

    reads input features in chunks

--- OCMFH/inputmatrices.py
+++ OCMFH/inputmatrices.py
@@ -15,23 +15,6 @@ from os.path import join
 from sklearn.manifold import TSNE
 
 epsilon = 0.00000001
-def zeroMeanRow(x):
-    return x - np.mean(x, 1).reshape((-1, 1))
-def zeroMean(x, isTest=False, train_mean=None):
-    if (isTest == False):
-        col_mean = np.mean(x, 0)
-        return x - col_mean, col_mean
-    else:
-        return x - train_mean
-def l2Normalize(x):
-    return normalize(x, 'l2')
-
-def tsne(x, num_samples, filename, title):
-    x_embedded = TSNE().fit_transform(x[:num_samples, :])
-    plt.scatter(x_embedded[:, 0], x_embedded[:, 1], s=2)
-    plt.title(title + ', {} samples'.format(num_samples))
-    plt.savefig(filename)
-    plt.clf()
 
 class inputMatrices:
     """
@@ -44,47 +27,14 @@ class inputMatrices:
         """ Declares various input matrices. 
             E.g. train_xv for storing training image features
         """
-        self.train_xv = None
-        self.train_xt = None
-        self.train_y = None
-        self.test_xv = None
-        self.test_xt = None
-        self.test_y = None
-        self.xv_anchors = None
-        self.xt_anchors = None
+        self.X1_train = None
+        self.X2_train = None
+        self.Y_train = None
+        self.X1_test = None
+        self.X2_test = None
+        self.Y_test = None
         # helps in map calculation
         self.num_relevant_items = None
-
-    def zeroMeanL2Normalize(self):
-        """apply zero mean L2 norm to input matrices"""
-        print('Applying zero mean L2 norm normalization...')
-        self.train_xv = zeroMeanRow(self.train_xv)
-        self.train_xv = l2Normalize(self.train_xv)
-        self.test_xv = l2Normalize(zeroMeanRow(self.test_xv))
-
-        self.train_xt = zeroMeanRow(self.train_xt)
-        self.train_xt = l2Normalize(self.train_xt)
-        self.test_xt = l2Normalize(zeroMeanRow(self.test_xt))
-
-    def zeroMeanUnitVariance(self):
-        """
-            apply zero mean unit variance to input matrices
-            Note: If in one of the dimension, standard deviation is
-                  zero then it is replaced with zero to avoid 
-                  divide by zero error.
-        """
-        print('Applying zero mean unit variance normalization...')
-        xv_mean = np.mean(self.train_xv, 0)
-        xt_mean = np.mean(self.train_xt, 0)
-        xv_std = np.std(self.train_xv, 0)
-        xv_std = np.where(xv_std==0, 1.0, xv_std)
-        xt_std = np.std(self.train_xt, 0)
-        xt_std = np.where(xt_std==0, 1.0, xt_std)
-
-        self.train_xv = (self.train_xv - xv_mean)/xv_std
-        self.test_xv = (self.test_xv - xv_mean)/xv_std
-        self.train_xt = (self.train_xt - xt_mean)/xt_std
-        self.test_xt = (self.test_xt - xt_mean)/xt_std
 
     def findNumRelevantItems(self):
         """
@@ -93,7 +43,7 @@ class inputMatrices:
             will be used in MAP and recall calculation later in model.py file.
             self.num_relevant_items.shape = (#test samples, )
         """
-        similarity_matrix = np.matmul(self.train_y, self.test_y.T)
+        similarity_matrix = np.matmul(self.Y_train, self.Y_test.T)
         similarity_matrix = np.where(similarity_matrix>0, 1.0, 0.0)
         self.num_relevant_items = np.sum(similarity_matrix, axis = 0)
 
@@ -102,12 +52,12 @@ class inputMatrices:
             This funtion prints type and shape of all input matrices on STDOUT.
             In is necessary that input features are of float type.
         """
-        print("Train image features:", self.train_xv.shape, type(self.train_xv[0, 0]))
-        print("Train text features:", self.train_xt.shape, type(self.train_xt[0, 0]))
-        print("Test image features:", self.test_xv.shape,  type(self.test_xv[0, 0]))
-        print("Test text features:", self.test_xt.shape, type(self.test_xt[0, 0]))
-        print("Train labels:", self.train_y.shape, type(self.train_y[0, 0]))
-        print("Test labels:", self.test_y.shape, type(self.test_y[0, 0]))
+        print("Train image features:", len(self.X1_train), self.X1_train[0].shape, type(self.X1_train[0][0, 0]))
+        print("Train text features:", len(self.X2_train), self.X2_train[0].shape, type(self.X2_train[0][0, 0]))
+        print("Test image features:", self.X1_test.shape,  type(self.X1_test[0, 0]))
+        print("Test text features:", self.X2_test.shape, type(self.X2_test[0, 0]))
+        print("Train labels:", self.Y_train.shape, type(self.Y_train[0, 0]))
+        print("Test labels:", self.Y_test.shape, type(self.Y_test[0, 0]))
 
 def loadFromNpy(filename):
     """load numpy variable from file"""
@@ -129,13 +79,14 @@ class NUS_WIDE(inputMatrices):
             dirpath_xt: path to AllTags1k.txt file
             dirpath_y: path to AllTags81.txt file
             num_concepts: consider num_concepts number of most frequent labels only
-            test_percent: how many percent of total dataset goes to test data
+            num_test_samples: number samples to consider in test set
             eliminate_unlabeled_samples: True if samples with no label associated with it
                                              has to be discarded.
                                         False otherwise
+            samples_per_chunk: number of samples in one chunk.
     """
     def __init__(self, dirpath_xv, dirpath_xt, dirpath_y,\
-                 num_concepts=81, test_percent=20, eliminate_unlabeled_samples=False):
+                 num_concepts=10, num_test_samples=2000, eliminate_unlabeled_samples=True, samples_per_chunk=5000):
         inputMatrices.__init__(self)
         print('reading input matrices...')
         image_features = np.loadtxt(dirpath_xv + 'BoW_int.dat')
@@ -157,16 +108,31 @@ class NUS_WIDE(inputMatrices):
             text_features = text_features[valid_item_indices, :]
 
         print('spliting into training and testing data...')
-        num_test_samples = (ground_truths.shape[0] * test_percent)//100
         # sampling n random numbers from a range without replacement
         test_indices = sorted(random.sample(range(0, ground_truths.shape[0]), num_test_samples))
         train_indices = [i for i in range(0, ground_truths.shape[0]) if i not in test_indices]
-        self.train_xv = image_features[train_indices, :]
-        self.test_xv = image_features[test_indices, :]
-        self.train_xt = text_features[train_indices, :]
-        self.test_xt = text_features[test_indices, :]
-        self.train_y = ground_truths[train_indices, :]
-        self.test_y = ground_truths[test_indices, :]
+        X1_train = image_features[train_indices, :]
+        X2_train = text_features[train_indices, :]
+        self.Y_train = ground_truths[train_indices, :]
+        self.X1_test = image_features[test_indices, :]
+        self.X2_test = text_features[test_indices, :]
+        self.Y_test = ground_truths[test_indices, :]
+
+        # creating chunks
+        print('creating chunks of atmost {} samples for training features only...'.format(samples_per_chunk))
+        total_samples = X1_train.shape[0]
+        num_chunks = (total_samples//samples_per_chunk) + ((total_samples % samples_per_chunk) != 0)
+        curr_index = 0
+        self.X1_train = []
+        self.X2_train = []
+        for i in range(num_chunks):
+            self.X1_train.append( X1_train[curr_index: curr_index + samples_per_chunk, :].copy())
+            self.X2_train.append( X2_train[curr_index: curr_index + samples_per_chunk, :].copy())
+            curr_index += samples_per_chunk
+
+        # deleting redundent data
+        del X1_train
+        del X2_train
 
 class NUS_WIDE_Lite(inputMatrices):
     """
@@ -189,22 +155,22 @@ class NUS_WIDE_Lite(inputMatrices):
         print('Reading image features...')
         train_feature_file = 'Normalized_CORR_Lite_Train.dat'
         test_feature_file = 'Normalized_CORR_Lite_Test.dat'
-        self.train_xv = np.loadtxt(dirpath_xv + train_feature_file)
-        self.test_xv = np.loadtxt(dirpath_xv + test_feature_file)
+        self.X1_train = np.loadtxt(dirpath_xv + train_feature_file)
+        self.X1_test = np.loadtxt(dirpath_xv + test_feature_file)
 
     def updateTrainTestTextFeatures(self, dirpath_xt):
         print('Reading text features...')
         train_text_file = 'Lite_Tags1k_Train.txt'
         test_text_file = 'Lite_Tags1k_Test.txt'
-        self.train_xt = np.loadtxt(dirpath_xt + train_text_file)
-        self.test_xt = np.loadtxt(dirpath_xt + test_text_file)
+        self.X2_train = np.loadtxt(dirpath_xt + train_text_file)
+        self.X2_test = np.loadtxt(dirpath_xt + test_text_file)
 
     def updateTrainTestLabels(self, dirpath_y):
         print('Reading ground truth labels...')
         train_label_file = 'Lite_Tags81_Train.txt'
         test_label_file = 'Lite_Tags81_Test.txt'
-        self.train_y = np.loadtxt(dirpath_y + train_label_file)
-        self.test_y = np.loadtxt(dirpath_y + test_label_file)
+        self.Y_train = np.loadtxt(dirpath_y + train_label_file)
+        self.Y_test = np.loadtxt(dirpath_y + test_label_file)
 
 # following main function is for debugging purpose
 if __name__ == '__main__':
