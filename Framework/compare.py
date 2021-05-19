203f4a69574fc8c9d705f601b3860dd2bf588452
Prabhat <prabhatkanaujia08@gmail.com>
2021-05-19 15:34:38 +0530

    Comparator

--- Framework/compare.py
+++ Framework/compare.py
@@ -5,5 +5,15 @@ import random
 import sys
 import time
 import json
+import matplotlib.pyplot as plt
 
+class Comparator:
+    def __init__(self, file1, file2):
+        self.stats1 = np.load(file1, allow_pickle=True)[()]
+        self.stats2 = np.load(file2, allow_pickle=True)[()]
+        self.label1 = file1.split('.')[0]
+        self.label2 = file2.split('.')[0]
+        self.comparisions = {}
+        
 
+    def compareParams(self):
